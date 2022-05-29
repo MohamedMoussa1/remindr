d97da4526417cf27c7d8c51ec0ddf6fa71636dff
roozbehR <61944934+roozbehR@users.noreply.github.com>
2022-05-29 03:43:36 -0400

    Corrected the module path in some componenets

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -17,10 +17,10 @@ import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from '../listItems/listItems';
-import Chart from '../Chart/Chart';
-import Deposits from '../Deposits/Deposits';
-import Orders from '../Orders/Orders';
+import { mainListItems, secondaryListItems } from './listItems';
+import Chart from './Chart';
+import Deposits from './Deposits';
+import Orders from './Orders';
 
 function Copyright(props) {
   return (
