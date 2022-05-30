5ce54084a1a053f177056ba5ae352c3af652f6a3
Mohamed Moussa <mohamed.moussa@mail.utoronto.ca>
2022-05-29 21:34:18 -0400

    partial signin setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
+import "./App.css";
+import SignInSide from "./components/SignInSide";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<SignInSide />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
